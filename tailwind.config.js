799f07edc6da77e05ca936c539b869f9d1b368fb
Andreas Seit <a.seitaridis@certh.gr>
2021-10-27 17:10:13 +0300

    Previous tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
-  purge: {
-    layers: ['base'],
-    content: [
-      './app/**/*.html.erb',
-      './app/helpers/**/*.rb',
-    ]
-  },
+  purge: [
+
+    './app/**/*.html.erb',
+    './app/helpers/**/*.rb',
+
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
