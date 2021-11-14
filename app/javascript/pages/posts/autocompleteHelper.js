f6b0a7345e7c32f930721bc06983a3b787415cc9
Andreas Seit <a.seitaridis@certh.gr>
2021-11-14 13:01:37 +0200

    Refactored autocompleteHelper

--- app/javascript/pages/posts/autocompleteHelper.js
+++ app/javascript/pages/posts/autocompleteHelper.js
@@ -7,6 +7,10 @@ import TagHelper from "./tagHelper";
 // TODO: When tag not exists create the tag
 export default class AutocompleteHelper {
 
+    ADD_TAGS_INPUT_MESSAGE = 'Click to add tags'
+    LIMIT_REACHED_INPUT_MESSAGE = 'You cannot add more tags'
+
+
     constructor(form, tagsContainer) {
         this.selectedTags = []
         this.form = form
@@ -55,29 +59,25 @@ export default class AutocompleteHelper {
         const obj = this
         const selectedTags = obj.selectedTags
 
-        if (selectedTags.length === 5) {
-            console.log('Already 5 tags')
-        } else {
+        if (!this._reachedTagLimit()) {
             const id = selectedItem.id
             const name = selectedItem.name
-            const existingIndex = selectedTags.findIndex(h => h.label === name)
-            if (existingIndex > -1) {
-                console.log('Already exists')
-            } else {
-                // create a new tag helper object
-                const tagHelper = new TagHelper(id, name, null, selectedTags.length, this.form, this.tagsContainer)
-                // add to selected tags
-                selectedTags.push(tagHelper)
 
-                $(tagHelper.xButton).on('remove-label', function () {
-                    selectedTags.splice(tagHelper.index, 1)
-                    obj._updateTagHelpersIndexes()
-                    if(obj.selectedTags.length < 5)
-                        obj.tagsInput.attr('placeholder', 'Click to add tags')
-                })
-                obj.tagsInput.attr('placeholder', 'You cannot add more tags')
-            }
+            const tagHelper = new TagHelper(id, name, null, selectedTags.length, this.form, this.tagsContainer)
+            selectedTags.push(tagHelper)
+
+            $(tagHelper.xButton).on('remove-label', function () {
+                selectedTags.splice(tagHelper.index, 1)
+                obj._updateTagHelpersIndexes()
+                if(obj.selectedTags.length < 5)
+                    obj._enableInput()
+            })
+
+            if (this._reachedTagLimit())
+                this._disableInput()
+
         }
+
         this._clearInput()
     }
 
@@ -114,6 +114,24 @@ export default class AutocompleteHelper {
 
     _clearInput () {
         this.tagsInput.val('')
+    }
+
+    _disableInput () {
+        this.tagsInput.attr('disabled', 'disabled')
+        this.tagsInput.attr('placeholder', this.LIMIT_REACHED_INPUT_MESSAGE)
+    }
+
+    _enableInput () {
+        this.tagsInput.removeAttr('disabled')
+        this._resetPlaceholder()
+    }
+
+    _resetPlaceholder () {
+        this.tagsInput.attr('placeholder', this.ADD_TAGS_INPUT_MESSAGE)
+    }
+
+    _reachedTagLimit () {
+        return this.selectedTags.length === 5
     }
 
 }
